394e048571f8da02b69711d1b258a0a1b12190bb
lilSageBrush <69616668+lilSageBrush@users.noreply.github.com>
2020-09-10 22:36:39 -0600

    initial commit
    basic HTML and JavaScript.

---
+++ Project1_expressions_alert/JS/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+window.alert("Hello World!");
+var a = "I love peaches,", b = " don\'t you?"; // Here I declare mutiple variables in one line, place a comma after the value of the var to start making a new one //
+var a = a.fontcolor("red"); // Both this line and the one below set the color of a string. //
+var b = b.fontcolor("blue");
+document.write(a + b); // This will add text to an html document. //
+var c = (32 + 8); // A Simple mathmatical expression. //
